031202950cdaa765c44dd57e49480f4a3cf7300d
Yuval Shalom <yuvalysh0@gmail.com>
2021-10-31 23:36:57 +0200

    changes

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -42,21 +42,28 @@
     <q-page-container>
       <div class="about" id="about">
         <div class="about-box">
-          <h1 class="primary-text primary-about">About Me</h1>
-          <p class="secondary-text">
-            <img class="profile-pic" src="../assets/profilePic.jpg">
-            My name is Yuval Shalom, I’m 23 years old from <span>Bat-Yam, Israel.</span>
-            <br>
-            I’m a full stack web developer.<br>
-            After I finished my <span>military service,</span> I wanted to do something meaningful and enjoyable,
-            so I started programming.
-            <br>
-            I decided to study <span>computer science</span> at HIT.
-            <br>
-            After one year of studying I took a year brake from the degree so I can
-            bring my programming skills to another level.
-          </p>
 
+          <h1 class="primary-text primary-about">About Me</h1>
+
+          <div class="display-flex">
+            <p class="secondary-text">
+              My name is Yuval Shalom, I’m 23 years old from <span><a href="https://en.wikipedia.org/wiki/Bat_Yam" target="_blank">Bat-Yam, Israel.</a></span>
+              <br>
+              I play football as a striker in <span><a href="https://www.football.org.il/players/player/?player_id=233242" target="_blank">Maccabi-Yavne F.C.</a></span><br>
+              Oh and I’m also a <span><a href="https://www.linkedin.com/in/yuval-shalom-8b64921bb/" target="_blank">full stack web developer</a></span><br>
+              After I finished my <span><a href="https://en.wikipedia.org/wiki/Military_Police_Corps_(Israel)" target="_blank">military service,</a></span> I wanted to do something meaningful and enjoyable,
+              so I started programming.
+              <br>
+              I decided to study <span><a href="https://www.youtube.com/watch?v=N775KsWQVkw" target="_blank">computer science</a></span> at HIT.
+              <br>
+              After one year of studying I took a year brake from the degree so I can
+              bring my programming skills to <span><a href="https://www.youtube.com/watch?v=S-AGE8rDs3Q" target="_blank">another level.</a></span>
+            </p>
+            <div class="profile-picture-container">
+              <img class="profile-pic" src="../assets/profilePic.jpg" alt="profile">
+              <div class="profile-picture-border"></div>
+            </div>
+          </div>
           <a href="#" class="btn btn-primary btn-animate">My Resume</a>
 
           <h1 class="primary-text primary-skills">The technologies I use: </h1>
@@ -131,7 +138,7 @@
               <p class="project-text-primary">FriendsBook</p>
               <p class="project-text-secondary">A social network built with Vuejs, Vuex, Css, Quasar framework FireBase, Nodejs and Express.</p>
             </div>
-            <a href="#" class="btn-mini btn-primary-mini btn-animate">Github</a>
+            <a href="https://github.com/yuvalysh0/Friendsbook" target="_blank" class="btn-mini btn-primary-mini btn-animate">Github</a>
             <a href="#" class="btn-mini btn-primary-mini btn-animate">Demo</a>
           </div>
 
@@ -140,7 +147,7 @@
               <p class="project-text-primary">Flash Media Scheduler</p>
               <p class="project-text-secondary">This app was built by the flashdev students and I (4 people). We used Vuejs, Vuex, Quasar framework, Firebase, Css, Bitbucket and Jira.</p>
             </div>
-            <a href="#" class="btn-mini btn-primary-mini btn-animate">Github</a>
+            <a href="#" target="_blank" class="btn-mini btn-primary-mini btn-animate">Github</a>
             <a href="#" class="btn-mini btn-primary-mini btn-animate">Demo</a>
           </div>
 
@@ -149,7 +156,7 @@
               <p class="project-text-primary">My Portfolio</p>
               <p class="project-text-secondary">A personal portfolio that was built with Vuejs, Css and Quasar framework</p>
             </div>
-            <a href="#" class="btn-mini btn-primary-mini btn-animate">Github</a>
+            <a href="https://github.com/yuvalysh0/yuval-portfolio" target="_blank" class="btn-mini btn-primary-mini btn-animate">Github</a>
             <a href="#projects" class="btn-mini btn-primary-mini btn-animate" @click="openDialog = true">Demo</a>
             <q-dialog v-model="openDialog">
               <q-card dark class="q-pa-md">
